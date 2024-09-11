8d24b53d6215e226e91569cde3acb6885e0cfff2
GuilhermeAGoncalves <guilhermegpop@gmail.com>
2024-09-11 03:12:13 -0300

    fix path login

--- partiw-front/src/service/users/UsersService.ts
+++ partiw-front/src/service/users/UsersService.ts
@@ -5,7 +5,7 @@ const API_URL = "http://54.152.151.234:8080/api";
 
 export default {
   login: async (email: String, password: String): Promise<LoginResponse> => {
-    const response = await axios.post(`${API_URL}/api/users/login`, {
+    const response = await axios.post(`${API_URL}/users/login`, {
       email,
       password,
     });
